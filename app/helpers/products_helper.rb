dbc3bb9356a553148f8962705b8d2f5c9b494e7d
Nudar <nnudar@gmail.com>
2013-07-26 15:25:29 -0400

    Finished iteration 3

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -5,9 +5,11 @@ module ProductsHelper
 
     def print_stock(stock)
         if stock > 0
-            
+         content_tag(:span, "In Stock", :class => 'in_stock')
+         # "<span class='in_stock'>In Stock (##)</span>"   
         else
-
+         content_tag(:span, "Out of Stock", :class => 'out_stock')
+          #{}"<span class='out_stock'>Out of Stock</span>"
         end
     end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
-    validates_numericality_of :price
-
+    validates_numericality_of :price, :order_id, :product_id
+    has_many :order_items
     def price=(input)
         input.delete!("$")
         super
