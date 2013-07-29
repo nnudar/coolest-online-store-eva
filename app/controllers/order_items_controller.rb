867b58838b5624c457aee601c337dd9495d33adc
Nudar <nnudar@gmail.com>
2013-07-29 11:17:17 -0400

    Finished iteration 3

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -65,10 +65,10 @@ class OrderItemsController < ApplicationController
   private
     def load_order
       @order = Order.find_or_initialize_by_id(session[:order_id], status: "unsubmitted")
-        if @order.new_record?
-          @order.save!
-          session[:order_id] = @order.id
-        end
+      if @order.new_record?
+        @order.save!
+        session[:order_id] = @order.id
+      end
     end
 
 
